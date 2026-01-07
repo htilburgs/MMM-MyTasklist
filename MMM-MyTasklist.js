d18773e58731d6b4510b9c43be0ae27420f1514c
Harm Tilburgs <harm@tilburgs.com>
2026-01-07 22:39:14 +0100

    Update MMM-MyTasklist.js

--- MMM-MyTasklist.js
+++ MMM-MyTasklist.js
@@ -1,14 +1,15 @@
 Module.register("MMM-MyTasklist", {
   defaults: {
-    updateInterval: 300000,
-    showCompleted: true,
-    maxTasks: null
+    updateInterval: 300000, // 5 minuten
+    showCompleted: true,    // toon voltooide taken
+    maxTasks: null          // null = geen limiet, anders bijv. 5
   },
 
   start() {
     this.tasks = [];
     this.sendSocketNotification("GET_TASKS");
 
+    // Periodieke update
     this.updateTimer = setInterval(() => {
       this.sendSocketNotification("GET_TASKS");
     }, this.config.updateInterval);
@@ -35,13 +36,18 @@ Module.register("MMM-MyTasklist", {
     }
 
     const ul = document.createElement("ul");
+
+    // Filter op showCompleted
     let visibleTasks = this.tasks.filter(task => this.config.showCompleted || !task.done);
 
+    // Limiteer aantal taken als maxTasks is ingesteld
     if (this.config.maxTasks && visibleTasks.length > this.config.maxTasks) {
       visibleTasks = visibleTasks.slice(0, this.config.maxTasks);
     }
 
+    // Taken toevoegen
     visibleTasks.forEach(task => ul.appendChild(this.createTaskElement(task)));
+
     wrapper.appendChild(ul);
     return wrapper;
   },
