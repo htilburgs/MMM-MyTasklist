42aaf6e27bb61fca71cfaeb059548b062cb09e3a
Harm Tilburgs <harm@tilburgs.com>
2026-01-07 18:48:12 +0100

    Update MMM-MyTasklist.js

--- MMM-MyTasklist.js
+++ MMM-MyTasklist.js
@@ -1,19 +1,15 @@
 Module.register("MMM-MyTasklist", {
   defaults: {
-    tasksFile: "tasks.json",    // JSON-bestand met taken
-    updateInterval: 300000,     // 5 minuten
-    showCompleted: true,        // toon voltooide taken
-    maxTasks: null              // null = geen limiet
+    tasksFile: "tasks.json",
+    updateInterval: 300000,
+    showCompleted: true,
+    maxTasks: null
   },
 
   start() {
     this.tasks = [];
-
-    // Stuur bestandspad naar helper
     this.sendSocketNotification("INIT", this.config.tasksFile);
     this.sendSocketNotification("GET_TASKS");
-
-    // Periodieke update
     this.startUpdateTimer();
   },
 
@@ -52,22 +48,17 @@ Module.register("MMM-MyTasklist", {
 
     const ul = document.createElement("ul");
 
-    // Filter op showCompleted
     let visibleTasks = this.tasks.filter(
       task => this.config.showCompleted || !task.done
     );
 
-    // Sorteer onafgeronde taken eerst
     visibleTasks.sort((a, b) => a.done - b.done);
 
-    // Limiteer aantal taken
     if (Number.isInteger(this.config.maxTasks)) {
       visibleTasks = visibleTasks.slice(0, this.config.maxTasks);
     }
 
-    // Taken toevoegen
     visibleTasks.forEach(task => ul.appendChild(this.createTaskElement(task)));
-
     wrapper.appendChild(ul);
     return wrapper;
   },
@@ -80,11 +71,9 @@ Module.register("MMM-MyTasklist", {
     checkbox.type = "checkbox";
     checkbox.checked = task.done;
 
-    // Optimistische UI-update
     checkbox.addEventListener("change", () => {
       task.done = checkbox.checked;
       this.updateDom(200);
-
       this.sendSocketNotification("TOGGLE_TASK", task.id);
     });
 
